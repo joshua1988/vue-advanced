cf6167c1f09273277926d20f8a244c82a79a785b
joshua1988 <jangkeehyo@gmail.com>
2019-02-25 10:53:09 +0900

    async await 실습을 위한 초기 코드

---
+++ async-await/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button @click="loginUser">login</button>
+    <h1>List</h1>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      items: [],
+    }
+  },
+  methods: {
+    loginUser() {
+      axios.get('https://jsonplaceholder.typicode.com/users/1')
+        .then(response => console.log(response))
+        .catch(error => console.log(error));
+    },
+  },
+}
+
+</script>
