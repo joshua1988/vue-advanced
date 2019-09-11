183dff4612bf5ea15840fd3e99d5ffffb79aa53b
zaien24 <48939071+zaien24@users.noreply.github.com>
2019-09-11 14:13:10 +0900

    섹션 5. 프로젝트 셋업
    # Vue CLI로 프로젝트 생성 및 ESLint 로그 확인

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -1,20 +1,27 @@
 <template>
   <div id="app">
-    <ToolBar></ToolBar>
-    <router-view></router-view>
+    hello
   </div>
-  <!-- test -->
 </template>
 
 <script>
-import ToolBar from './components/ToolBar.vue'
 
 export default {
-  components: {
-    ToolBar,
+  methods: {
+    fetchData() {
+      console.log('hello');
+    }
   }
 }
 </script>
 
 <style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
